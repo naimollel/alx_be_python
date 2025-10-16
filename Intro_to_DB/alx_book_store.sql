a0e632113080b892473ef77e55440e70d64bcbef
Nai Emmanuel Mollel <51174372+naimollel@users.noreply.github.com>
2025-10-16 12:39:33 +0300

    0. A Magical Database for Your Dream Online Bookstore Adventure!

---
+++ Intro_to_DB/alx_book_store.sql
@@ -0,0 +1,47 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the database
+USE alx_book_store;
+
+-- Create Authors table
+CREATE TABLE AUTHORS (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create Books table
+CREATE TABLE BOOKS (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+-- Create Customers table
+CREATE TABLE CUSTOMERS (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+-- Create Orders table
+CREATE TABLE ORDERS (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+-- Create Order_Details table
+CREATE TABLE ORDER_DETAILS (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
